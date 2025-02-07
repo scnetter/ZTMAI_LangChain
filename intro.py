00e464f814a5f152715905b3bd6d86ceb67eb07b
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2025-02-07 15:11:18 -0600

    Add messages list for system, assistant, human message content

--- intro.py
+++ intro.py
@@ -6,5 +6,19 @@ import openai
 load_dotenv(find_dotenv(), override=True)
 
 llm = ChatOpenAI()
-output = llm.invoke('Explain quantum mechanics in one sentence')
+//output = llm.invoke('Explain quantum mechanics in one sentence')
+
+from langchain.schema import(
+    SystemMessage, # System
+    AIMessage, # Assistant
+    HumanMessage, # user
+)
+
+messages = [
+    SystemMessage(content='You are a physicist and respond only in German.'),
+    HumanMessage(content='Can you explain quantum mechanics in one sentence?'),
+    # AImessage will be returned from llm.invoke()
+]
+
+output = llm.invoke(messages)
 print(output.content)
